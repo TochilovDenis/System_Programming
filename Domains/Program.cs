493a834f6a9f6df363741a59e49ce6b0f1360548
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-19 20:27:53 +0300

    Получим имя и базовый каталог текущего домена и выведем все загруженные в домен сборки

---
+++ Domains/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domains
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AppDomain domain = AppDomain.CurrentDomain;
+            Console.WriteLine($"FriendlyName: {domain.FriendlyName}");
+            Console.WriteLine($"Base Directory: {domain.BaseDirectory}");
+            Console.WriteLine();
+
+            Assembly[] assemblies = domain.GetAssemblies();
+            foreach (Assembly asm in assemblies)
+            {
+                Console.WriteLine(asm.GetName().Name);
+            }
+        }
+    }
+}
