7b27255efa9ccecf761b8185a8e130601d5bf025
TochilovDenis <denis-tochilov@yandex.ru>
2025-02-19 20:58:42 +0300

    Пример код работы Многопоточность

---
+++ Multithreading/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Multithreading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            void Print() => Console.WriteLine("Hello Threads");
+
+            Thread myThread = new Thread(Print);
+
+            for(int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Главный поток: {i}");
+                Thread.Sleep(300);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine($"Второй поток: {i}");
+                Thread.Sleep(300);
+            }
+        }
+    }
+}
